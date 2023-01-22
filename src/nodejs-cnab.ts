52c1335882c959a74348ad33f26cefed14cacbfb
Daniel <madureiradaniel600@gmail.com.br>
2023-01-22 12:05:05 -0300

    gerando retorno atráves de json

--- src/nodejs-cnab.ts
+++ src/nodejs-cnab.ts
@@ -1,11 +1,12 @@
 import { BANK } from './const'
 import { generateRemessaCnab } from './remessa'
-import { parseRemessaCnab } from './retorno'
+import { parseRemessaCnab, generateRetornoCnab } from './retorno'
 import { helperGenerateRemessaCNAB240 } from './helperGenerateRemessaCNAB240'
 import { helperGenerateRemessaCNAB400 } from './helperGenerateRemessaCNAB400'
 
 export {
   generateRemessaCnab,
+  generateRetornoCnab,
   parseRemessaCnab,
   BANK,
   helperGenerateRemessaCNAB240,
--- src/retorno.ts
+++ src/retorno.ts
@@ -1,4 +1,4 @@
-import { makeLine, readYaml } from './utils'
+import { makeLine, readYaml, readLine } from './utils'
 import { CNAB_YAML_DIR } from './const'
 
 /**
@@ -50,5 +50,49 @@ export const parseRemessaCnab = (
     return infos
   } catch (e) {
     console.error(`parseRemessaCnab: `, e)
+  }
+}
+
+/**
+ * GERAR ARQUIVO RETORO
+ * @param {*} files
+ * @param {*} cnabtype
+ * @param {*} bankcode
+ */
+export const generateRetornoCnab = (files: any, cnabtype = 400, bankcode = '237') => {
+  try {
+    const yamls: any = []
+    for (const key in files) {
+      const value = files[key]
+      if (value.forEach) {
+        value.forEach((v: any) => {
+          const layout = readYaml(CNAB_YAML_DIR + `/cnab${cnabtype}/${bankcode}/retorno/${key}.yml`)
+          yamls.push({
+            layout,
+            data: v
+          })
+        })
+      } else {
+        const layout = readYaml(CNAB_YAML_DIR + `/cnab${cnabtype}/${bankcode}/retorno/${key}.yml`)
+        yamls.push({
+          layout,
+          data: value
+        })
+      }
+    }
+
+    const infos = yamls.map((i: any, index: any) => {
+      return readLine(i.layout, i.data)
+    })
+
+    const infosLine = infos.map((i: any) => {
+      return i.line
+    })
+
+    const CNAB_EOL = '\r\n'
+    const data = infosLine.join(CNAB_EOL)
+    return data
+  } catch (e) {
+    console.error(`generateRemessaCnab`, e)
   }
 }
